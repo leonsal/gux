fdfab389f7b2bd34fdd8ceb5b2b97608d3e0f9a3
leonsal <leonel.salvadori@gmail.com>
2022-12-02 13:08:46 -0300

    ...

--- canvas/line_test.go
+++ canvas/line_test.go
@@ -16,9 +16,23 @@ func TestLine(t *testing.T) {
 	}
 
 	c := New()
-	points := []gb.Vec2{{100, 500}, {500, 500}, {500, 900}}
+	//points := []gb.Vec2{{100, 500}, {500, 500}, {500, 900}}
 	//points := []gb.Vec2{{100, 500}, {500, 500}}
+	points := []gb.Vec2{
+		{100, 500}, {200, 100},
+		{300, 500}, {400, 100},
+		{500, 500}, {600, 100},
+		{700, 500}, {800, 100},
+	}
 	c.polyLineTextured(points, 0xFF_00_00_00, 0, 10)
+
+	points2 := []gb.Vec2{
+		{100, 900}, {200, 500},
+		{300, 900}, {400, 500},
+		{500, 900}, {600, 500},
+		{700, 900}, {800, 500},
+	}
+	c.polyLineTextured(points2, 0xFF_00_00_00, 0, 10)
 	for win.StartFrame(0) {
 		win.RenderFrame(&c.DrawList)
 	}

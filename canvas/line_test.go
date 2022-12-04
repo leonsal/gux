3fe611729c3da4479269112676bd2a27b1edf41f
leonsal <leonel.salvadori@gmail.com>
2022-12-04 09:03:45 -0300

    ...

--- canvas/line_test.go
+++ canvas/line_test.go
@@ -15,26 +15,21 @@ func TestLine(t *testing.T) {
 		panic(err)
 	}
 
+	// Translate and scale the supplied array of points
+	transform := func(points []gb.Vec2, trans gb.Vec2, scale float32) {
+		for i := range points {
+			(&points[i]).Add(trans).MultScalar(scale)
+		}
+	}
+
 	c := New()
-	//points := []gb.Vec2{{100, 500}, {500, 500}, {500, 900}}
-	//points := []gb.Vec2{{100, 500}, {500, 500}}
-	//c.AddPolyLineAntiAliased(points, 0xFF_00_00_00, 0, 10)
+	points := []gb.Vec2{{0, 10}, {10, 0}, {20, 10}, {30, 0}, {40, 10}, {50, 0}, {60, 10}}
+	transform(points, gb.Vec2{10, 0}, 20)
 
-	points1 := []gb.Vec2{
-		{100, 500}, {200, 100},
-		{300, 500}, {400, 100},
-		{500, 500}, {600, 100},
-		{700, 500}, {800, 100},
+	for width := 1; width < 30; width += 4 {
+		c.AddPolyLineAntiAliased(points, 0xFF_00_00_00, 0, float32(width))
+		transform(points, gb.Vec2{0, 100}, 1)
 	}
-	c.AddPolyLineAntiAliased(points1, 0xFF_00_00_FF, 0, 10)
-
-	points2 := []gb.Vec2{
-		{100, 900}, {200, 500},
-		{300, 900}, {400, 500},
-		{500, 900}, {600, 500},
-		{700, 900}, {800, 500},
-	}
-	c.AddPolyLineTextured(points2, 0xFF_00_00_00, 0, 10)
 
 	for win.StartFrame(0) {
 		win.RenderFrame(&c.DrawList)

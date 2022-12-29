4344ba9e241b57ed7fb6f62774b1aae332a1ae46
leonsal <leonel.salvadori@gmail.com>
2022-12-29 15:51:06 -0300

    ..

--- gb/gb_test.go
+++ gb/gb_test.go
@@ -18,86 +18,77 @@ func Test1(t *testing.T) {
 	var rect [1]RGBA
 	rect[0] = MakeColor(255, 255, 255, 255)
 
-	// Creates and transfer texture
+	// Creates and transfer 1 pixel opaque white texture needed for all commands
 	texId := win.CreateTexture(1, 1, &rect[0])
 
 	// DrawList 1
 	drawList1 := DrawList{}
-	cmd1, idxBuf1, vtxBuf1 := drawList1.NewDrawCmd(6, 4)
-	cmd1.ClipRect = Vec4{0, 0, 4000, 4000}
-	cmd1.TexID = texId
-	idxBuf1[0] = 0
-	idxBuf1[1] = 1
-	idxBuf1[2] = 2
-	idxBuf1[3] = 2
-	idxBuf1[4] = 3
-	idxBuf1[5] = 0
-	vtxBuf1[0] = Vertex{Pos: Vec2{10, 10}, Col: 0xFF_FF_00_00}
-	vtxBuf1[1] = Vertex{Pos: Vec2{10, 100}, Col: 0xFF_FF_00_00}
-	vtxBuf1[2] = Vertex{Pos: Vec2{200, 100}, Col: 0xFF_FF_00_00}
-	vtxBuf1[2] = Vertex{Pos: Vec2{200, 10}, Col: 0xFF_FF_00_00}
+	{
+		cmd, idxBuf, vtxBuf := drawList1.NewDrawCmd(6, 4)
+		cmd.ClipRect = Vec4{0, 0, 4000, 4000}
+		cmd.TexID = texId
+		idxBuf[0] = 0
+		idxBuf[1] = 1
+		idxBuf[2] = 2
+		idxBuf[3] = 2
+		idxBuf[4] = 3
+		idxBuf[5] = 0
+		vtxBuf[0] = Vertex{Pos: Vec2{10, 10}, Col: 0xFF_FF_00_00}
+		vtxBuf[1] = Vertex{Pos: Vec2{10, 100}, Col: 0xFF_FF_00_00}
+		vtxBuf[2] = Vertex{Pos: Vec2{200, 100}, Col: 0xFF_FF_00_00}
+		vtxBuf[3] = Vertex{Pos: Vec2{200, 10}, Col: 0xFF_FF_00_00}
+	}
+	{
+		cmd, idxBuf, vtxBuf := drawList1.NewDrawCmd(6, 4)
+		cmd.ClipRect = Vec4{0, 0, 4000, 4000}
+		cmd.TexID = texId
+		idxBuf[0] = 0
+		idxBuf[1] = 1
+		idxBuf[2] = 2
+		idxBuf[3] = 2
+		idxBuf[4] = 3
+		idxBuf[5] = 0
+		vtxBuf[0] = Vertex{Pos: Vec2{500, 0}, Col: 0xFF_00_00_FF}
+		vtxBuf[1] = Vertex{Pos: Vec2{500, 250}, Col: 0xFF_00_00_FF}
+		vtxBuf[2] = Vertex{Pos: Vec2{750, 250}, Col: 0xFF_00_00_FF}
+		vtxBuf[3] = Vertex{Pos: Vec2{750, 0}, Col: 0xFF_00_00_FF}
+	}
 
-	cmd2, idxBuf2, vtxBuf2 := drawList1.NewDrawCmd(6, 4)
-	cmd2.ClipRect = Vec4{0, 0, 4000, 4000}
-	cmd2.TexID = texId
-	idxBuf2[0] = 0
-	idxBuf2[1] = 1
-	idxBuf2[2] = 2
-	idxBuf2[3] = 2
-	idxBuf2[4] = 3
-	idxBuf2[5] = 0
-	vtxBuf2[0] = Vertex{Pos: Vec2{500, 0}, Col: 0xFF_00_00_FF}
-	vtxBuf2[1] = Vertex{Pos: Vec2{500, 250}, Col: 0xFF_00_00_FF}
-	vtxBuf2[2] = Vertex{Pos: Vec2{750, 250}, Col: 0xFF_00_00_FF}
-	vtxBuf2[3] = Vertex{Pos: Vec2{750, 0}, Col: 0xFF_00_00_FF}
+	// DrawList 2
+	drawList2 := DrawList{}
+	{
+		cmd, idxBuf, vtxBuf := drawList1.NewDrawCmd(3, 3)
+		cmd.ClipRect = Vec4{0, 0, 4000, 4000}
+		cmd.TexID = texId
+		idxBuf[0] = 0
+		idxBuf[1] = 1
+		idxBuf[2] = 2
+		vtxBuf[0] = Vertex{Pos: Vec2{200, 800}, Col: 0xFF_00_00_FF}
+		vtxBuf[1] = Vertex{Pos: Vec2{400, 800}, Col: 0xFF_00_FF_00}
+		vtxBuf[2] = Vertex{Pos: Vec2{300, 600}, Col: 0xFF_FF_00_FF}
+	}
+	{
+		cmd, idxBuf, vtxBuf := drawList1.NewDrawCmd(3, 3)
+		cmd.ClipRect = Vec4{0, 0, 4000, 4000}
+		cmd.TexID = texId
+		idxBuf[0] = 0
+		idxBuf[1] = 1
+		idxBuf[2] = 2
+		vtxBuf[0] = Vertex{Pos: Vec2{700, 800}, Col: 0xFF_00_00_FF}
+		vtxBuf[1] = Vertex{Pos: Vec2{900, 800}, Col: 0xFF_00_FF_00}
+		vtxBuf[2] = Vertex{Pos: Vec2{800, 500}, Col: 0xFF_FF_00_FF}
+	}
 
-	//	drawList1.AddCmd(Vec4{}, texId,
-	//		[]uint32{0, 1, 2, 2, 3, 0},
-	//		[]Vertex{
-	//			Vertex{Pos: Vec2{10, 10}, Col: 0xFF_FF_00_00},
-	//			Vertex{Pos: Vec2{10, 100}, Col: 0xFF_FF_00_00},
-	//			Vertex{Pos: Vec2{200, 100}, Col: 0xFF_FF_00_00},
-	//			Vertex{Pos: Vec2{200, 10}, Col: 0xFF_FF_00_00},
-	//		},
-	//	)
-	//	drawList1.AddCmd(Vec4{}, texId,
-	//		[]uint32{0, 1, 2, 2, 3, 0},
-	//		[]Vertex{
-	//			Vertex{Pos: Vec2{500, 0}, Col: 0xFF_00_00_FF},
-	//			Vertex{Pos: Vec2{500, 250}, Col: 0xFF_00_00_FF},
-	//			Vertex{Pos: Vec2{750, 250}, Col: 0xFF_00_00_FF},
-	//			Vertex{Pos: Vec2{750, 0}, Col: 0xFF_00_00_FF},
-	//		},
-	//	)
-	//
-	//	// DrawList 2
-	//	drawList2 := DrawList{}
-	//	drawList2.AddCmd(Vec4{}, texId,
-	//		[]uint32{0, 1, 2},
-	//		[]Vertex{
-	//			Vertex{Pos: Vec2{200, 800}, Col: 0xFF_00_00_FF},
-	//			Vertex{Pos: Vec2{400, 800}, Col: 0xFF_00_FF_00},
-	//			Vertex{Pos: Vec2{300, 600}, Col: 0xFF_FF_00_00},
-	//		},
-	//	)
-	//	drawList2.AddCmd(Vec4{}, texId,
-	//		[]uint32{0, 1, 2},
-	//		[]Vertex{
-	//			Vertex{Pos: Vec2{700, 800}, Col: 0xFF_00_00_FF},
-	//			Vertex{Pos: Vec2{900, 800}, Col: 0xFF_00_FF_00},
-	//			Vertex{Pos: Vec2{800, 500}, Col: 0xFF_FF_00_00},
-	//		},
-	//	)
-	//
-	//	// Create new DrawList from concatenation of DrawList1 and 2
-	//	drawList := DrawList{}
-	//	drawList.AddList(drawList1)
-	//	drawList.AddList(drawList2)
+	// Create new DrawList from concatenation of DrawList1 and 2
+	drawList := DrawList{}
+	drawList.AddList(drawList1)
+	drawList.AddList(drawList2)
 
 	// Render loop
 	frames := 0
 	cgoCallsStart := runtime.NumCgoCall()
 	frameParams := FrameParams{}
+	frameParams.ClearColor = Vec4{0.5, 0.5, 0.5, 1}
 	for {
 		finfo := win.StartFrame(&frameParams)
 		if finfo.WinClose {
